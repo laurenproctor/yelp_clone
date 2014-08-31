6cc0542da3b02e97ea816fb64911bcf13b42316a
laurenproctor <laurenproctor32@gmail.com>
2014-08-31 15:01:34 -0400

    creating Yelp base skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpClone::Application.config.secret_key_base = '99b9adbeaa935bc2fa995d5d4cf45804f78c2fbde37074ffc31eb8c52a38a1779b8f8cbfddc1666d9c6a795124793f1fe1f245cf585c9a8819a7c20d2b1f797d'
