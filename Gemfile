2764923258e9955ec07b21233e501f6bb49f0157
laurenproctor <laurenproctor32@gmail.com>
2014-08-31 17:02:41 -0400

    Adding image upload with carrierwave s3 and figaro

--- Gemfile
+++ Gemfile
@@ -33,6 +33,10 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'carrierwave'
+gem 'fog', "~> 1.3.1"
+gem 'figaro'
+
 gem 'rails_12factor', group: :production
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831175519) do
+ActiveRecord::Schema.define(version: 20140831193557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140831175519) do
     t.string   "website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
